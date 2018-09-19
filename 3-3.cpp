9c39b93032c2a4ce44fa527b72ccb7ad15206bdb
MK12SS <32871093+MK12SS@users.noreply.github.com>
2018-09-19 14:21:48 +0800

    comments

--- 3-3.cpp
+++ 3-3.cpp
@@ -1,3 +1,22 @@
+/*
+3-3 Jump Game(2018/9/19(We-3))
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Determine if you are able to reach the last index.
+Input
+First line contains a single integer N(1<=N<=100 0000), which is the length of array. Second line contains N integers, splited by spaces.
+Output
+If the last index is reacheable, print string "yes\n", else print "no\n".
+Example
+Input
+    5
+    2 3 1 1 4
+output
+    yes
+Explanation
+    Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+*/
 #include<iostream>
 using namespace std;
 int main()
